99f4cb2cc2929a07a7007c5cd0495a2ba612a385
pirvudoru <pirvu.doru@ymail.com>
2017-10-23 21:01:26 +0300

    Add Builder exercise

---
+++ DesignPatternExercise/Creational/Builder/UrlFormatter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatternExercise.Creational.Builder
+{
+    public class UrlFormatter
+    {
+        public string Format(string baseUrl, Dictionary<string, string> query)
+        {
+            var queryString = FormatQueryString(query);
+
+            return string.IsNullOrEmpty(queryString) ? baseUrl : $"{baseUrl}?{queryString}";
+        }
+
+        public string Format(string baseUrl, string hash)
+        {
+            return string.IsNullOrEmpty(hash) ? baseUrl : $"{baseUrl}#{hash}";
+        }
+
+        public string Format(string baseUrl, Dictionary<string, string> query, string hash)
+        {
+            var queryString = FormatQueryString(query);
+            var result = baseUrl;
+
+            if (!string.IsNullOrEmpty(queryString))
+            {
+                result = $"{result}?{queryString}";
+            }
+            if (!string.IsNullOrEmpty(hash))
+            {
+                result = $"{result}#{hash}";
+            }
+
+            return result;
+        }
+
+        private string FormatQueryString(Dictionary<string, string> query)
+        {
+            var queryValues = query.Select((item) => $"{item.Key}={Uri.EscapeUriString(item.Value)}");
+
+            return string.Join("&", queryValues);
+        }
+    }
+}
---
+++ DesignPatternExerciseTests/Creational/Builder/EntitlementsTests.cs
@@ -0,0 +1,88 @@
+﻿using DesignPatternExercise.Creational.Builder;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DesignPatternExerciseTests.Creational.Builder
+{
+    [TestFixture]
+    public class UrlTests
+    {
+        private UrlFormatter _subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _subject = new UrlFormatter();
+        }
+
+        [Test]
+        public void Format_WithBaseUrlAndEmptyQuery_FormatsUrlWithoutQueryString()
+        {
+            var baseUrl = "https://example.com";
+            var query = new Dictionary<string, string>();
+
+            var result = _subject.Format(baseUrl, query);
+
+            Assert.AreEqual("https://example.com", result);
+        }
+
+        [Test]
+        public void Format_WithBaseUrlAndQuery_FormatsUrlWithQueryString()
+        {
+            var baseUrl = "https://example.com";
+            var query = new Dictionary<string, string> { { "term", "some term" }, { "page", "1" } };
+
+            var result = _subject.Format(baseUrl, query);
+
+            Assert.AreEqual("https://example.com?term=some%20term&page=1", result);
+        }
+
+        [Test]
+        public void Format_WithBaseUrlAndHash_FormatsUrlWithHash()
+        {
+            var baseUrl = "https://example.com";
+            var query = new Dictionary<string, string> { { "term", "42" } };
+            var hash = "foo";
+
+            var result = _subject.Format(baseUrl, hash);
+
+            Assert.AreEqual("https://example.com#foo", result);
+        }
+
+        [Test]
+        public void Format_WithBaseUrlQueryAndHash_FormatsUrlWithQueryAndHash()
+        {
+            var baseUrl = "https://example.com/x/y";
+            var query = new Dictionary<string, string> { { "term", "test term" } };
+            var hash = "foo";
+
+            var result = _subject.Format(baseUrl, query, hash);
+
+            Assert.AreEqual("https://example.com/x/y?term=test%20term#foo", result);
+        }
+
+        [Test]
+        public void Format_WithBaseUrlQueryAndWithEmptyHash_FormatsUrlWithQueryAndWithoutHash()
+        {
+            var baseUrl = "https://example.com/x/y";
+            var query = new Dictionary<string, string> { { "term", "test term" } };
+            var hash = "";
+
+            var result = _subject.Format(baseUrl, query, hash);
+
+            Assert.AreEqual("https://example.com/x/y?term=test%20term", result);
+        }
+
+        [Test]
+        public void Format_WithBaseUrlQueryAndHash_FormatsUrlWithHashAndWithoutQuery()
+        {
+            var baseUrl = "https://example.com/x/y";
+            var query = new Dictionary<string, string>();
+            var hash = "foo";
+
+            var result = _subject.Format(baseUrl, query, hash);
+
+            Assert.AreEqual("https://example.com/x/y#foo", result);
+        }
+    }
+}
